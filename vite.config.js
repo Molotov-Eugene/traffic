09b7ec39643def5fc74c10f6633c44c7df3bd9a1
Eugene Molotov <forever_air@Eugenes-MacBook-Air.local>
2023-12-05 00:10:52 +0500

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export default defineConfig(({ command, mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
   return (
     {
       plugins: [react()],
-      define: {
-        VITE_KV_REST_API_URL: JSON.stringify(env.KV_REST_API_URL),
-        VITE_KV_REST_API_TOKEN: JSON.stringify(env.KV_REST_API_TOKEN),
-
-        // KV_REST_API_URL: JSON.stringify(env.KV_REST_API_URL),
-        // KV_REST_API_TOKEN: JSON.stringify(env.KV_REST_API_TOKEN),
-        'process.env': {},
-      }
     }
   )
 });
