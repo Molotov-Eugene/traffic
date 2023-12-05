69c03a12b5e841a495c9d5039aaad92e20969c85
Eugene Molotov <forever_air@Eugenes-MacBook-Air.local>
2023-12-05 14:38:21 +0500

    is it over?

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    errorElement: <ErrorPage />,
+    errorElement: <MainLayout />,
     children: [
       {
         path: 'events',
